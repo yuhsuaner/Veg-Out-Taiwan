42715febbeab1d8a5d0f50cb7efff93965897d4f
yuhsuaner <yuhsunaer@gmail.com>
2020-03-20 15:15:43 +0800

    add CollectionView basic setting

--- Veg Out Taiwan/Controller/VeganMap/MapViewController.swift
+++ Veg Out Taiwan/Controller/VeganMap/MapViewController.swift
@@ -15,6 +15,11 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //hide NavigationBar
+        let barAppearance =  UINavigationBarAppearance()
+        barAppearance.configureWithTransparentBackground()
+        navigationController?.navigationBar.standardAppearance = barAppearance
+        
         //Google Map SDK: COMPASS
         mapView.settings.compassButton = true
         
@@ -22,12 +27,60 @@ class MapViewController: UIViewController {
         mapView.isMyLocationEnabled = true
         mapView.settings.myLocationButton = true
         
+        mapView.addSubview(collectionView)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = UIColor.W1
+        
+        NSLayoutConstraint.activate([
+            collectionView.widthAnchor.constraint(equalToConstant: 64),
+            collectionView.widthAnchor.constraint(equalTo: collectionView.heightAnchor),
+            collectionView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            collectionView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        ])
     }
+    
+    fileprivate var collectionView: UICollectionView = {
+       
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        
+        let collectionView  = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(RestaurantCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.showsHorizontalScrollIndicator = false
+        return collectionView
+    }()
 }
 
 extension MapViewController: CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
+    }
+}
+
+extension MapViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? RestaurantCollectionViewCell else { return UICollectionViewCell() }
+        
+        cell.backgroundColor = UIColor.DG
+        
+        return cell
+    }
+    
+}
+
+extension MapViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: collectionView.frame.width/2.5, height: collectionView.frame.width/2)
     }
 }
---
+++ Veg Out Taiwan/View/VeganMap/RestaurantCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  RestaurantCollectionViewCell.swift
+//  Veg Out Taiwan
+//
+//  Created by Irene.C on 2020/3/20.
+//  Copyright © 2020 Irene.C. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantCollectionViewCell: UICollectionViewCell {
+    
+}
